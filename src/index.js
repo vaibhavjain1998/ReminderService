7c359b38a2eb779274f0e4cea6e56991a2b49966
Vaibhav <vjain0998@gmail.com>
2023-05-23 19:51:32 +0530

    Setup of SMTP using nodemailer done

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
 
+const { sendBasicEmail } = require('./services/email-service');
+
 const setupAndStartServer = () => {
     const app = express();
     app.use(bodyParser.json());
@@ -9,6 +11,13 @@ const setupAndStartServer = () => {
 
     app.listen(PORT, () => {
         console.log(`Server started at ${PORT}`);
+
+        sendBasicEmail(
+            'support@admin.com',
+            'formalsmart01@gmail.com',
+            'This is a testing email',
+            'Hey, I hope you like the support'
+        );
     });
 }
 
